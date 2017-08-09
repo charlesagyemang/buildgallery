0a52785cae2d8867f37e89610fb9442e48640ec6
charles agyemang <micnkru@gmail.com>
2017-08-09 19:28:09 +0000

    Initial commit with auth and all

--- script/app.js
+++ script/app.js
@@ -4,12 +4,6 @@
 
 
 $(document).ready(function(){
-  //alert("hey")
 
-  // Get from data
-  params = {related: "*"};
-  SDK.queryData("plans", "plans_table", params, function(response){
-    console.log(response)
-  });
 
 })
